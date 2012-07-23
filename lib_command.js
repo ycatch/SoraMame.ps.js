8caf6d6279a5586f9ec5712b3bc19e1cdb994116
ycatch <y-catch@ja2.so-net.ne.jp>
2012-07-24 00:54:44 +0900

    図形を追加

--- lib_command.js
+++ lib_command.js
@@ -4,8 +4,19 @@
 function lib_command() {
 
   this.dic = {
-	マル : 'ellipse',
+  	//Structure
 	サイズ : 'size',
+
+	//2D Primitives
+	マル : 'ellipse',
 	直線 : 'line',
+	テン : 'point',
+	三角 : 'triangle',
+
+	//Mouse 
+	マウスX : 'mouseX',
+	マウスY : 'mouseY',
+
+	//test
 	test : '//SoraMame Script!'};
 }
