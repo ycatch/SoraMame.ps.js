51e331f4080d9834699fe0eb4555090b03a2ddf7
ycatch <y-catch@ja2.so-net.ne.jp>
2012-07-28 02:36:06 +0900

    Processingに固有のグローバル変数(システム変数)を 変換可能にする（mouseXとか）

---
+++ lib_global_vars.js
@@ -0,0 +1,23 @@
+﻿/* Soramame.ps.js から Processing.jsへの変換辞書
+*/
+
+function lib_global_vars() {
+
+  this.dic = {
+	//Mouse 
+	マウスX : 'mouseX',
+	マウスY : 'mouseY',
+
+	//test
+	test : '//SoraMame!'};
+}
+
+lib_global_vars.prototype.get = function(str){
+	var msg;
+    if ( this.dic[str] === undefined ) {
+		msg = str;
+	} else {
+    	msg = this.dic[str];
+    }
+	return msg;
+}
